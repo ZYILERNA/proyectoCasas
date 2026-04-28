ce86933df71bb8710c5dfeb8a45979ebbde8f518
ZYILERNA <zhenyang2006@gmail.com>
2026-04-28 22:09:19 +0200

    Password2

--- app/api/visits/route.js
+++ app/api/visits/route.js
@@ -5,13 +5,13 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(request) {
   const supabase = createClient(
-    'https://nmcnyrjwbbhhzbajztfs.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tY255cmp3YmJoaHpiYWp6dGZzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjEyNDU0MCwiZXhwIjoyMDc3NzAwNTQwfQ.t8RzAXqUBCo8K9WpI_W76GxCDYVzY1Ft9LGywHW123M'
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
   );
   const { searchParams } = new URL(request.url);
   const password = searchParams.get('pw');
 
-  if (password !== 'wonly2024') {
+  if (password !== 'wonly2025') {
     return NextResponse.json({ error: 'No autorizado' }, { status: 401 });
   }
 
